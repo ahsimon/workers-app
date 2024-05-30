4d9216a9604e6666d533bb90d9e6efac89687067
ahsimon <ahsimon.us@gmail.com>
2024-05-30 03:40:05 -0400

    chore(counter)  enabled delayed counter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { WorkerService } from './worker.service';
+import { time } from 'console';
 
 @Controller()
 export class AppController {
@@ -16,10 +17,10 @@ export class AppController {
       const numRequests = Number(count) || 10000;
 
       try {
-
-            this.workerService.callApiAsync("https://jsonplaceholder.typicode.com/todos/",numRequests);
-  
-          return { 'status':'ok' };
+            const start = Date.now();
+            await this.workerService.callApiAsync("https://jsonplaceholder.typicode.com/todos/",numRequests);
+            const  end = (Date.now() - start)/1000;
+          return { 'status':'ok' , 'time in seconds'  : end};
       } catch (error) {
           return { error: error.message };
       }
